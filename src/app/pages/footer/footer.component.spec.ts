345dbb95be8e2c5f4eac03bdc00de56c167ad592
Gaetan LAVIT <g.lavit@outlook.fr>
2023-08-17 12:02:53 +0400

    version de partage 0

--- src/app/pages/footer/footer.component.spec.ts
+++ src/app/pages/footer/footer.component.spec.ts
@@ -6,10 +6,12 @@ describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [FooterComponent]
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FooterComponent ]
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
