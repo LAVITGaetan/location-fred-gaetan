5a0ff890f27f1d3dfe4e27d6d404ebf593338985
Gaetan LAVIT <g.lavit@outlook.fr>
2023-08-24 11:02:18 +0400

    version de partage 0.44

--- src/app/carts/cartshort/cartshort.component.ts
+++ src/app/carts/cartshort/cartshort.component.ts
@@ -5,29 +5,40 @@ export interface Orders {
   name: string;
   nameRoute: string;
   price: number;
-  priceTotal: number;
   promo: number;
   quantity: number;
   thumbnail: string;
   weight: number
-  weightTotal: number;
 }
 
-const ELEMENT_DATA: Orders[] = [
+const order: Orders[] = [
   {
     name: 'Tente Qaou V5 pack 3en1',
     nameRoute: 'Tente-Qaou-V5-pack-3en1',
-    price: 179, priceTotal: 179,
+    price: 179,
     promo: 179,
     quantity: 1,
     thumbnail: '../../assets/images/product/qaou/Qaou-Sleeping-pad2-300x300.webp',
     weight: 2.1,
-    weightTotal: 2.1
   },
-  { name: 'Tente Qaou V5 pack 6en1', nameRoute: 'Tente-Qaou-V5-pack-6en1', price: 239, priceTotal: 239, promo: 239, quantity: 1, thumbnail: '../../assets/images/product/qaou/Qaou-Sleeping-pad2-300x300.webp', weight: 3.3, weightTotal: 3.3 },
-  { name: 'Hamac double toit', nameRoute: 'Qaou-Hamac-double-toi-vert', price: 29, priceTotal: 29, promo: 29, quantity: 1, thumbnail: '../../assets/images/product/qaou/Qaou-Sleeping-pad2-300x300.webp', weight: 0.6, weightTotal: 0.6 },
-  { name: 'Matelas trek gonflable', nameRoute: 'Qaou-Matelas-Randonnée',  price: 45, priceTotal: 45 , promo: 45, quantity: 1,   thumbnail: '../../assets/images/product/qaou/Qaou-Sleeping-pad2-300x300.webp' , weight: 0.65, weightTotal : 0.65},
+  { name: 'Tente Qaou V5 pack 6en1', nameRoute: 'Tente-Qaou-V5-pack-6en1', price: 239, promo: 239, quantity: 1, thumbnail: '../../assets/images/product/qaou/Qaou-Sleeping-pad2-300x300.webp', weight: 3.3 },
+  { name: 'Hamac double toit', nameRoute: 'Qaou-Hamac-double-toi-vert', price: 29, promo: 29, quantity: 1, thumbnail: '../../assets/images/product/qaou/Qaou-Sleeping-pad2-300x300.webp', weight: 0.6 },
+  { name: 'Matelas trek gonflable', nameRoute: 'Qaou-Matelas-Randonnée', price: 45, promo: 45, quantity: 1, thumbnail: '../../assets/images/product/qaou/Qaou-Sleeping-pad2-300x300.webp', weight: 0.65 },
 ];
+
+export interface Panier {
+  id: number,
+  date_start: string,
+  date_end: string,
+  products: Orders[]
+}
+
+const panier:  Panier = {
+  id: 1,
+  date_start: '1212',
+  date_end: '1414',
+  products: order
+}
 
 @Component({
   selector: 'app-cartshort',
@@ -36,13 +47,14 @@ const ELEMENT_DATA: Orders[] = [
 })
 export class CartshortComponent {
   longueur: number = 0;
-  items = ELEMENT_DATA;
+  items = panier.products;
   totalTtc = 0;
-  WeightTotal = 0;
+  totalWeight = 0;
   codePromo = 'Camping974'
+  panier = panier
 
   ngOnInit() {
-    this.totalTtc = this.items.map(item => item.priceTotal).reduce((prev, curr) => prev + curr, 0);
+    this.totalTtc = this.items.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
   }
 
   constructor(
@@ -61,14 +73,14 @@ export class CartshortComponent {
   }
 
   calculateOrder(index: number) {
-    this.items[index].weightTotal = this.items[index].weight * this.items[index].quantity;
-    this.items[index].priceTotal = this.items[index].price * this.items[index].quantity;
+    this.items[index].weight = this.items[index].weight * this.items[index].quantity;
+    this.items[index].price = this.items[index].price * this.items[index].quantity;
     /* une decimal pour le poids */
-    this.items[index].weightTotal = Math.floor(this.items[index].weightTotal * 10) / 10
+    this.items[index].weight = Math.floor(this.items[index].weight * 10) / 10
     /* deux decimal pour le prix */
-    this.items[index].priceTotal = Math.floor(this.items[index].priceTotal * 100) / 100
+    this.items[index].price = Math.floor(this.items[index].price * 100) / 100
     /* Calcul du prix total */
-    this.totalTtc = this.items.map(item => item.priceTotal).reduce((prev, curr) => prev + curr, 0);
+    this.totalTtc = this.items.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
     this.totalTtc = Math.floor(this.totalTtc * 100) / 100
 
   }
@@ -86,7 +98,7 @@ export class CartshortComponent {
   }
 
   calcSum() {
-    this.totalTtc = this.items.map(item => item.priceTotal).reduce((prev, curr) => prev + curr, 0);
+    this.totalTtc = this.items.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
 
   }
 }
